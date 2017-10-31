3c78adf95ce97d11a4c3c8ed65afdc990fadaf63
Colin Atkinson <colin.william.atkinson@gmail.com>
2017-10-31 19:46:41 -0400

    Create login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import { Grid, Row, Col } from 'react-bootstrap';
+import { Grid } from 'react-bootstrap';
+
+import {
+    BrowserRouter as Router,
+    Route
+} from 'react-router-dom';
+
+import Login from './Login';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <Grid>
-                    <Row>
-                        <Col xs="6"><h1>Roomable</h1></Col>
-                    </Row>
-                    <Row>
-                        <Col xs="6">Leave no room for doubt</Col>
-                    </Row>
+                    <Router>
+                        <Route exact path="/" component={Login}/>
+                    </Router>
                 </Grid>
             </div>
         );
