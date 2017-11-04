17ffeb1e53bdec58b0bc678dfb1c8e8cbbb50915
Colin Atkinson <colin.william.atkinson@gmail.com>
2017-11-04 14:18:24 -0400

    Add 404 page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { Grid } from 'react-bootstrap';
 
 import {
     BrowserRouter as Router,
+    Switch,
     Route
 } from 'react-router-dom';
 
@@ -12,6 +13,7 @@ import Home from './Home';
 import Login from './Login';
 import Register from './Register';
 import Matches from './Matches';
+import NotFound from './NotFound';
 
 import sample_matches from './sample_matches';
 
@@ -22,12 +24,15 @@ class App extends Component {
             <div className="App">
                 <Router>
                     <Grid>
-                        <Route exact path="/" component={Home}/>
-                        <Route exact path="/login" component={Login}/>
-                        <Route exact path="/register" component={Register}/>
-                        <Route exact path="/matches" render={(props) => (
-                            <Matches {...props} matches={sample_matches} />
-                        )}/>
+                        <Switch>
+                            <Route exact path="/" component={Home}/>
+                            <Route exact path="/login" component={Login}/>
+                            <Route exact path="/register" component={Register}/>
+                            <Route exact path="/matches" render={(props) => (
+                                <Matches {...props} matches={sample_matches} />
+                            )}/>
+                            <Route component={NotFound} />
+                        </Switch>
                     </Grid>
                 </Router>
             </div>
---
+++ frontend/src/NotFound.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import { Col, PageHeader } from 'react-bootstrap';
+
+class NotFound extends Component {
+    render() {
+        return (
+            <div className="NotFound">
+                <Col xs={8} xsOffset={2}>
+                    <PageHeader>404 <small>That's an error</small></PageHeader>
+                    <h4>Try putting in a <i>correct</i> URL this time.</h4>
+                </Col>
+            </div>
+        );
+    }
+}
+
+export default NotFound;
