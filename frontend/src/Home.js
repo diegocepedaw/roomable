6b3bd240945e4de2bb1b6dab0403b952fdcca294
Colin Atkinson <colin.william.atkinson@gmail.com>
2017-11-04 14:29:12 -0400

    Fix Login page layout

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -5,7 +5,7 @@ import { Col, ButtonGroup, Button } from 'react-bootstrap';
 class Home extends Component {
     render() {
         return (
-            <div className="Login">
+            <div className="Home">
                 <Col sm={6} smOffset={3} md={4} mdOffset={4}>
                     <h1>Roomable</h1>
                     <h3>Leave no room for doubt</h3>
