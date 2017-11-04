5580f8a343a5d3223a04732e206baeb93238a3e2
diegocepedaw <diegocepedaw@gmail.com>
2017-11-03 22:40:18 -0400

    added basic match percentage calculation

--- backend/server/views.py
+++ backend/server/views.py
@@ -23,9 +23,21 @@ def index(request):
         if (user.email == compUser.email):
             continue
 
-        # calculate
+        # compare target users preferences to other user's attributes
 
+        pypref = serializers.serialize( "python", Preferences.objects.filter(email=targetemail) )
+        pyattr = serializers.serialize( "python", Attributes.objects.filter(email=user.email) )
 
+        # calculate match percentage
+        questionCount = 0.0
+        correctCount = 0.0
+        for (k,v), (k2,v2) in zip(pypref[0]['fields'].items(), pyattr[0]['fields'].items()):
+            questionCount += 1.0
+            if(v == v2):
+                correctCount +=1.0
+
+        matchPercentage = (correctCount/questionCount)*100
+        
         #serialize query results
         attr = json.loads(serializers.serialize('json', Attributes.objects.filter(email=user.email)))
         pref = json.loads(serializers.serialize('json', Preferences.objects.filter(email=user.email)))
@@ -33,12 +45,9 @@ def index(request):
 
 
 
-        data = OrderedDict([('email',usr[0]['fields']['email']), ('handle',usr[0]['fields']['handle']),('description',usr[0]['fields']['description']), ('attributes',attr[0]['fields']), ('preferences',pref[0]['fields'])])
+        data = OrderedDict([('email',usr[0]['fields']['email']), ('handle',usr[0]['fields']['handle']),('description',usr[0]['fields']['description']), ('attributes',attr[0]['fields']), ('preferences',pref[0]['fields']), ('match',matchPercentage)])
 
-        for key, value in data.items():
-            if key == 'email':
-                continue
-            print value
+
 
         matchesjson.append(data)
 
