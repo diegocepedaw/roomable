9cd0538c2a4e36afa5683bc5c64255f46b9af398
Colin Atkinson <colin.william.atkinson@gmail.com>
2017-11-05 16:18:24 -0500

    Remove redux stuff from OtherProfile

--- frontend/src/actions.js
+++ frontend/src/actions.js
@@ -1,34 +1,9 @@
 export const GET_TOKEN = 'GET_TOKEN';
-export const MATCHES_REQUEST = 'MATCHES_REQUEST';
-export const MATCHES_SUCCESS = 'MATCHES_SUCCESS';
-export const MATCHES_ERROR = 'MATCHES_ERROR';
-export const OTHER_REQUEST = 'OTHER_REQUEST';
-export const OTHER_SUCCESS = 'OTHER_SUCCESS';
-export const OTHER_ERROR = 'OTHER_ERROR';
 
 export function getToken(token, email) {
     return {
         type: GET_TOKEN,
         token,
         email
-    };
-}
-
-export function otherRequest() {
-    return {
-        type: OTHER_REQUEST
-    };
-}
-
-export function otherSuccess(data) {
-    return {
-        type: OTHER_SUCCESS,
-        data
-    };
-}
-
-export function otherError() {
-    return {
-        type: OTHER_ERROR
     };
 }
--- frontend/src/reducers.js
+++ frontend/src/reducers.js
@@ -2,20 +2,11 @@ import { combineReducers } from 'redux';
 
 import {
     GET_TOKEN,
-    OTHER_REQUEST,
-    OTHER_SUCCESS,
-    OTHER_ERROR
 } from './actions';
 
 const initialToken = {
     token: null,
     email: null,
-};
-
-const initialUser = {
-    data: null,
-    loading: false,
-    error: false,
 };
 
 function token(state = initialToken, action) {
@@ -32,40 +23,8 @@ function token(state = initialToken, action) {
     }
 }
 
-function otherUser(state = initialUser, action) {
-    switch (action.type) {
-        case OTHER_REQUEST: {
-            return {
-                ...state,
-                data: null,
-                loading: true,
-                error: false
-            };
-        }
-        case OTHER_SUCCESS: {
-            return {
-                ...state,
-                data: action.data,
-                loading: false,
-                error: false,
-            };
-        }
-        case OTHER_ERROR: {
-            return {
-                ...state,
-                data: null,
-                loading: false,
-                error: true
-            };
-        }
-        default:
-            return state;
-    }
-}
-
 const roomableApp = combineReducers({
     token,
-    otherUser
 });
 
 export default roomableApp;
