b97870cfa042ba919187005c96ba3ece4b5ac79e
Colin Atkinson <colin.william.atkinson@gmail.com>
2017-11-05 16:06:23 -0500

    Make Profile stateful

--- frontend/src/Profile.js
+++ frontend/src/Profile.js
@@ -1,18 +1,57 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { userRequest, userSuccess, userError } from './actions';
 import './Profile.css';
 
 
 import { Col, Form, Button, Jumbotron } from 'react-bootstrap';
 
 class Profile extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: null,
+            loading: false,
+            error: false,
+        };
+    }
+
     componentDidMount() {
-        this.props.dataFetch(this.props.email);
+        this.dataFetch(this.props.email);
+    }
+
+    dataFetch(email) {
+        this.setState({
+            matches: null,
+            loading: true,
+            error: false
+        });
+        fetch(`/server/api/getuser/${email}`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Bad response code');
+                } else {
+                    return response.json();
+                }
+            })
+            .then(json => {
+                this.setState({
+                    data: json,
+                    loading: false,
+                    error: false
+                });
+            })
+            .catch(() => {
+                this.setState({
+                    data: null,
+                    loading: false,
+                    error: true
+                });
+            });
     }
 
     render() {
-        if (this.props.error) {
+        if (this.state.error) {
             return (
                 <div className="Profile">
                     <Col xs={7} xsOffset={1}>
@@ -22,7 +61,7 @@ class Profile extends Component {
             );
         }
 
-        if (!this.props.data || this.props.loading) {
+        if (!this.state.data || this.state.loading) {
             return (
                 <div className="Profile">
                     <Col xs={7} xsOffset={1}>
@@ -36,21 +75,14 @@ class Profile extends Component {
             <div className="Profile">
                 <Col xs={7} xsOffset={1}>
                     <Jumbotron>
-                        <h1><i className="fa fa-user" aria-hidden="true"></i> {this.props.data.handle}</h1>
+                        <h1><i className="fa fa-user" aria-hidden="true"></i> {this.state.data.handle}</h1>
                         <hr />
-                        <p>{this.props.data.description}</p>
+                        <p>{this.state.data.description}</p>
                     </Jumbotron>
                     <Jumbotron>
                         <h1><i className="fa fa-address-book-o" aria-hidden="true"></i> Contact Links</h1>
                         <hr />
-                        <p><i className="fa fa-envelope" aria-hidden="true"></i> <a href={`mailto:${this.props.data.email}`}>{this.props.data.email}</a></p>
-                        <p><i className="fa fa-facebook" aria-hidden="true"></i><a href="https://facebook.com"> facebook</a></p>
-                        <p><i className="fa fa-twitter" aria-hidden="true"></i><a href="https://twitter.com"> twitter</a></p>
-                        <p>
-                            <Button type="submit">
-                                Edit Contact Links
-                            </Button>
-                        </p>
+                        <p><i className="fa fa-envelope" aria-hidden="true"></i> <a href={`mailto:${this.state.data.email}`}>{this.state.data.email}</a></p>
                     </Jumbotron>
                 </Col>
                 <Col xs={3} >
@@ -79,36 +111,10 @@ class Profile extends Component {
 
 const mapStateToProps = state => {
     return {
-        data: state.user.data,
-        loading: state.user.loading,
         email: state.token.email,
-        error: state.user.error,
-    };
-};
-
-const mapDispatchToProps = dispatch => {
-    return {
-        dataFetch: (email) => {
-            dispatch(userRequest());
-            fetch(`/server/api/getuser/${email}`)
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('Bad response code');
-                    } else {
-                        return response.json();
-                    }
-                })
-                .then(json => {
-                    dispatch(userSuccess(json));
-                })
-                .catch(() => {
-                    dispatch(userError());
-                });
-        }
     };
 };
 
 export default connect(
-    mapStateToProps,
-    mapDispatchToProps
+    mapStateToProps
 )(Profile);
